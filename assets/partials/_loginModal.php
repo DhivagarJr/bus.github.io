b995941a3ea17c4f680a1f481a6423acfb6522e3
DhivagarJr <dhivagar94bolt2002@gmail.com>
2022-11-10 16:40:36 +0530

    my bus booking website

---
+++ assets/partials/_loginModal.php
@@ -0,0 +1,43 @@
+<script>
+ function valid(){
+    var user_id = document.getElementById("username").value;
+    var pass_id = document.getElementById("password").value;
+    if(user_id ==""){
+      alert("Please enter your User ID...!");
+    }
+    if(pass_id ==""){
+      alert("Please enter your password...!");
+    }
+    else{
+      alert("Thankyou for login...!");
+    }
+  }
+  </script>
+<!-- Modal -->
+<div class="modal fade" id="loginModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="loginModalLabel">Login As Admin</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        <form action="assets/partials/_handleLogin.php" method="POST">
+          <div class="mb-3">
+              <label for="username" class="form-label">Username</label>
+              <input type="text" class="form-control" id="username" name="username" required>
+          </div>
+          <div class="mb-3">
+              <label for="password" class="form-label">Password</label>
+              <input type="password" class="form-control" id="password" name="password" required>
+              <div class="form-text">We'll never share your password with anyone else.</div>
+          </div>
+          <button type="submit" class="btn btn-success" name="submit" onclick="valid()" >Login</button>
+        </form>
+      </div>
+      <div class="modal-footer">
+        <!-- Add anything here in the future -->
+      </div>
+    </div>
+  </div>
+</div>
